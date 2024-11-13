f9e6a0fcc1cf5901f0c2840be5c1f98cb168e801
1abcdesign <one.abcdesign@gmail.com>
2024-11-13 10:02:08 +0200

    Updates

---
+++ assets/getPath-CR53BJmt.js
@@ -0,0 +1,1 @@
+function e(t){return t.includes("https")?t:"https://1abcdesign.github.io/"+t}export{e as g};
--- assets/getPath-CVtoei34.js
+++
@@ -1,1 +0,0 @@
-function e(t){return t.includes("https")?t:"https://abcdesign1.github.io/"+t}export{e as g};
