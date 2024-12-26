42b001789679fc8057d2d7e0ba8f2b0eb8d20b90
vadim4web <vadim4web@gmail.com>
2024-12-26 23:06:11 +0200

    Updates

--- assets/Logo3D-DJBOBzfK.js
+++ assets/Logo3D-DJBOBzfK.js
@@ -1,4 +1,4 @@
-import{r as dl,a as fl,G as pl,o as ml,c as gl}from"./index-BG6KUwj0.js";/**
+import{r as dl,a as fl,E as pl,o as ml,c as gl}from"./index-DSkHy5JG.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
