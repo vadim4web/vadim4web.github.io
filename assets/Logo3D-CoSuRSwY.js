aa4bb5f5794aae75854010f3cedbb5afa669659a
vadim4web <vadim4web@gmail.com>
2024-12-28 11:11:30 +0200

    Updates

--- assets/Logo3D-CoSuRSwY.js
+++ assets/Logo3D-CoSuRSwY.js
@@ -1,4 +1,4 @@
-import{r as dl,a as fl,E as pl,o as ml,c as gl}from"./index-Bg6oShVv.js";/**
+import{r as dl,a as fl,E as pl,o as ml,c as gl}from"./index-RudsVWD1.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
