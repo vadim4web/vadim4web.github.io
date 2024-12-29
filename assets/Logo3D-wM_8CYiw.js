ab919d84c77a06a80611d5645d76d006fff3b02c
vadim4web <vadim4web@gmail.com>
2024-12-29 11:22:59 +0200

    Updates

--- assets/Logo3D-wM_8CYiw.js
+++ assets/Logo3D-wM_8CYiw.js
@@ -1,4 +1,4 @@
-import{r as dl,a as fl,E as pl,o as ml,c as gl}from"./index-CTy2Uo1y.js";/**
+import{r as dl,a as fl,E as pl,o as ml,c as gl}from"./index-DAimL1be.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
