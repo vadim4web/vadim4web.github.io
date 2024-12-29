82d9b4815f947d6c759a33cd05957166feb5be4c
vadim4web <vadim4web@gmail.com>
2024-12-29 21:37:34 +0200

    Updates

--- assets/Logo3D-CwdDFRH2.js
+++ assets/Logo3D-CwdDFRH2.js
@@ -1,4 +1,4 @@
-import{r as dl,a as fl,E as pl,o as ml,c as gl}from"./index-CkEzXjpt.js";/**
+import{r as dl,a as fl,E as pl,o as ml,c as gl}from"./index-CYLle8x6.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
