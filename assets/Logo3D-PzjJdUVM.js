2d46944153d7548186c327bb88982d5d37f9c6b1
vadim4web <vadim4web@gmail.com>
2024-12-29 21:33:51 +0200

    Updates

--- assets/Logo3D-PzjJdUVM.js
+++ assets/Logo3D-PzjJdUVM.js
@@ -1,4 +1,4 @@
-import{r as dl,a as fl,E as pl,o as ml,c as gl}from"./index-BysNWize.js";/**
+import{r as dl,a as fl,E as pl,o as ml,c as gl}from"./index-CkEzXjpt.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
