ff7d2867c2a4a8418d40836c9fbd46a510647fa2
vadim4web <vadim4web@gmail.com>
2024-12-30 11:27:33 +0200

    Updates

--- assets/Logo3D-DqRl-9jE.js
+++ assets/Logo3D-DqRl-9jE.js
@@ -1,4 +1,4 @@
-import{r as dl,a as fl,E as pl,o as ml,c as gl}from"./index-NUx1pvTn.js";/**
+import{r as dl,a as fl,E as pl,o as ml,c as gl}from"./index-CJd0Ty_K.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
