16b017ced350dd1b47d76d555763f6909841d32b
vadim4web <vadim4web@gmail.com>
2024-12-30 10:37:24 +0200

    Updates

--- assets/Logo3D-CAfNlO4y.js
+++ assets/Logo3D-CAfNlO4y.js
@@ -1,4 +1,4 @@
-import{r as dl,a as fl,E as pl,o as ml,c as gl}from"./index-09lX8CdW.js";/**
+import{r as dl,a as fl,E as pl,o as ml,c as gl}from"./index-NUx1pvTn.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
