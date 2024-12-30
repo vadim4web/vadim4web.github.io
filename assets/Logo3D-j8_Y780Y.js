40a20c6f3f5a3e602915a369b30d0a6558deff0a
vadim4web <vadim4web@gmail.com>
2024-12-30 10:00:38 +0200

    Updates

--- assets/Logo3D-j8_Y780Y.js
+++ assets/Logo3D-j8_Y780Y.js
@@ -1,4 +1,4 @@
-import{r as dl,a as fl,E as pl,o as ml,c as gl}from"./index-N95FsTSv.js";/**
+import{r as dl,a as fl,E as pl,o as ml,c as gl}from"./index-Ds_QK-yB.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
