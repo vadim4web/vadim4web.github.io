2f501281f73a45d067de4c0f39e58d0023e982f1
vadim4web <vadim4web@gmail.com>
2024-12-30 11:32:03 +0200

    Updates

--- assets/Logo3D-Cfs_a4kO.js
+++ assets/Logo3D-Cfs_a4kO.js
@@ -1,4 +1,4 @@
-import{r as dl,a as fl,E as pl,o as ml,c as gl}from"./index-CJd0Ty_K.js";/**
+import{r as dl,a as fl,E as pl,o as ml,c as gl}from"./index-TC0kKIr1.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
