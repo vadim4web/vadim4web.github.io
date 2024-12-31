98864eafcc459335e565aa9b134f1fc750603916
vadim4web <vadim4web@gmail.com>
2024-12-31 23:36:55 +0200

    Updates

--- assets/Logo3D-BjXfX6S7.js
+++ assets/Logo3D-BjXfX6S7.js
@@ -1,4 +1,4 @@
-import{r as dl,a as fl,E as pl,o as ml,c as gl}from"./index-TC0kKIr1.js";/**
+import{r as dl,a as fl,E as pl,o as ml,c as gl}from"./index-CKTozw0J.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
