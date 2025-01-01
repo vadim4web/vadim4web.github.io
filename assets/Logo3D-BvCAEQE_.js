9fde4373e744c43ff291d499f8857593f2d477e1
vadim4web <vadim4web@gmail.com>
2025-01-01 05:11:13 +0200

    Updates

--- assets/Logo3D-BvCAEQE_.js
+++ assets/Logo3D-BvCAEQE_.js
@@ -1,4 +1,4 @@
-import{r as dl,a as fl,E as pl,o as ml,c as gl}from"./index-DjarhgMH.js";/**
+import{r as dl,a as fl,E as pl,o as ml,c as gl}from"./index-DY8SqUoD.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
