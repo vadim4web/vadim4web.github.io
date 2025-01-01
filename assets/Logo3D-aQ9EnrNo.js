7ebd3b558d2555a1e210e86516a144251950277b
vadim4web <vadim4web@gmail.com>
2025-01-01 03:24:12 +0200

    Updates

--- assets/Logo3D-aQ9EnrNo.js
+++ assets/Logo3D-aQ9EnrNo.js
@@ -1,4 +1,4 @@
-import{r as dl,a as fl,E as pl,o as ml,c as gl}from"./index-BTmyRCgS.js";/**
+import{r as dl,a as fl,E as pl,o as ml,c as gl}from"./index-DjarhgMH.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
