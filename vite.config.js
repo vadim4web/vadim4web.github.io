446c5c590a3263054924bfc18739cb8537694b05
vadim4web <vadim4web@gmail.com>
2025-01-19 19:02:31 +0200

    vite.config.js, router: remove VITE_BASE_URL related useless code

--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,31 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-export default defineConfig(({ command, mode }) => {
-	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
-
-	const isProduction = mode === 'production' || command === 'build'
-	const base = isProduction ? process.env.VITE_BASE_URL : '/'
-
-	return {
-		plugins: [vue()],
-		resolve: {
-			alias: {
-				'@': path.resolve(__dirname, 'src'),
-			},
-		},
-		base,
-		css: {
-			preprocessorOptions: {
-				scss: {
-					api: 'modern-compiler',
-				},
-			},
-		},
-		build: {
-			rollupOptions: {
-				output: {
-					manualChunks: {
-						three: ['three'],
-						gltfLoader: ['three/examples/jsm/loaders/GLTFLoader.js'],
-						dracoLoader: ['three/addons/loaders/DRACOLoader.js'],
-					},
-				},
-			},
-			chunkSizeWarningLimit: 550,
-		},
-	}
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          three: ['three'],
+          gltfLoader: ['three/examples/jsm/loaders/GLTFLoader.js'],
+          dracoLoader: ['three/addons/loaders/DRACOLoader.js'],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 550,
+  },
 })
