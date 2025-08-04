2f4697043f0841efc17ef600a405891ede6e3976
vadim4web <vadim4web@example.com>
2025-08-04 18:54:15 +0300

    Updates

--- assets/AboutPage-CuJzqLuE.js
+++
@@ -1,8 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/PageHeader--YOVRKTp.js","assets/index-205Ag54Y.js","assets/index-BBPhvpqK.css"])))=>i.map(i=>d[i]);
-import{_ as d,c as l,e as i,d as t,k as r,h as p,i as b,g as o,t as e,j as u,o as v}from"./index-205Ag54Y.js";const c={class:"flex-col"},m={class:"mission flex-col"},_={class:"mission-header"},f={class:"h2"},h={class:"t3 text1"},y={class:"t3 text2"},g={__name:"AboutPage",setup(w){const n=b(()=>u(()=>import("./PageHeader--YOVRKTp.js"),__vite__mapDeps([0,1,2])));return(s,a)=>(v(),l("main",c,[i(p(n),{"head-key":"aboutH21","text-key":"aboutT1"}),t("div",m,[t("div",_,[t("h2",f,[o(e(s.$t("aboutH221"))+" ",1),a[0]||(a[0]=t("br",null,null,-1)),o(" "+e(s.$t("aboutH222"))+" ",1),a[1]||(a[1]=t("br",null,null,-1)),o(" "+e(s.$t("aboutH223"))+" ",1),a[2]||(a[2]=t("br",null,null,-1)),o(" "+e(s.$t("aboutH224")),1)]),t("p",h,e(s.$t("aboutT21")),1),t("p",y,e(s.$t("aboutT22")),1)]),a[3]||(a[3]=t("div",{class:"photos w100 rel"},[t("div",{class:"photo photo1 w100"}),t("div",{class:"photo photo2 w100"})],-1))]),a[4]||(a[4]=r(`<pre class="rhymes text-center" data-v-b21348b7><span data-v-b21348b7>I&#39;ve earned my own very first PC</span>
-<span data-v-b21348b7>By working summer holidays in 2003</span>
-<span data-v-b21348b7>I&#39;m a \`web dev <em lang="es" data-v-b21348b7>loco</em>\` (if you will) code-addict</span>
-<span data-v-b21348b7>Devoted to development with JavaScript</span>
-<span data-v-b21348b7>I&#39;m experienced enough to exactly know</span>
-<span data-v-b21348b7>Using DOCs, AI, Google and Stackoverflow</span>
-    </pre>`,1))]))}},x=d(g,[["__scopeId","data-v-b21348b7"]]);export{x as default};
